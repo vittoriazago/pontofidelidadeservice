301a61103dc064472e75cfa67c823d6151759970
Vittoria Zago <vittoriazago1@hotmail.com>
2019-05-22 00:02:28 -0300

    Extensões para automapper e di

---
+++ PontoFidelidadeService/PontoFidelidadeService/Configs/AutoMapperConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace PontoFidelidade.WebApi
+{
+    public static class AutoMapperConfig
+    {
+        public static void ConfigureAutoMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(map =>
+            {
+
+
+            }, 
+            Assembly.GetCallingAssembly());
+        }
+    }
+}
---
+++ PontoFidelidadeService/PontoFidelidadeService/Configs/InjecaoDependenciaConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PontoFidelidade.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PontoFidelidade.WebApi
+{
+    public static class InjecaoDependenciaConfig
+    {
+        public static void ConfigureDI(this IServiceCollection services)
+        {
+
+            services.AddScoped(typeof(IRepositorio<>), typeof(RepositorioEntityFramework<>));
+
+        }
+    }
+}
