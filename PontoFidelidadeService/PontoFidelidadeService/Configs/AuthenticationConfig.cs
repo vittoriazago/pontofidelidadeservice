47ede32c07d1c30adfa68f7f72fa5643ec071d86
Vittoria Zago <vittoriazago1@hotmail.com>
2019-05-22 00:02:11 -0300

    Extensão para autenticação e autorização

---
+++ PontoFidelidadeService/PontoFidelidadeService/Configs/AuthenticationConfig.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using PontoFidelidade.Domain.Models;
+using PontoFidelidade.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PontoFidelidade.WebApi
+{
+    public static class AuthenticationConfig
+    {
+        public static void ConfigureAuthentication(this IServiceCollection services, string privateToken)
+        {
+
+            IdentityBuilder builder = services.AddIdentityCore<Usuario>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 4;
+            });
+            builder = new IdentityBuilder(builder.UserType, typeof(Role), builder.Services);
+            builder.AddEntityFrameworkStores<PontoFidelidadeContexto>();
+            builder.AddRoleValidator<RoleValidator<Role>>();
+            builder.AddRoleManager<RoleManager<Role>>();
+            builder.AddSignInManager<SignInManager<Usuario>>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                            .GetBytes(privateToken)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+        }
+    }
+}
