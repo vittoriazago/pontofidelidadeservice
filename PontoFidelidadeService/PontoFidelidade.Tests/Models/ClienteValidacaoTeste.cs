5b5abef6b9f130f634077a924d12a6f7e4b7ff27
Vittoria Zago <vittoriazago1@hotmail.com>
2019-05-22 00:31:30 -0300

    Primeiro teste

---
+++ PontoFidelidadeService/PontoFidelidade.Tests/Models/ClienteValidacaoTeste.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using PontoFidelidade.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PontoFidelidade.Tests.Models
+{
+    public class ClienteValidacaoTeste : TesteBaseValidacao
+    {
+
+        [Test]
+        public void Cliente_Validacao_Sucesso()
+        {
+            var novoCliente = new Cliente()
+            {
+                
+            };
+
+            Assert.AreEqual(0, ValidateModel(novoCliente).Count);
+        }
+    }
+}
