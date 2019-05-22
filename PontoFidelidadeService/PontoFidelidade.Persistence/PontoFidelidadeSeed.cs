8e4c7c9c38218ec5fae8670b0bf7e113308a8a00
VITTORIA ZAGO <vzago@paschoalotto.com.br>
2019-05-22 17:50:22 -0300

    Seeding ok

--- PontoFidelidadeService/PontoFidelidade.Persistence/PontoFidelidadeSeed.cs
+++ PontoFidelidadeService/PontoFidelidade.Persistence/PontoFidelidadeSeed.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace PontoFidelidade.Persistence
 {
@@ -10,8 +9,7 @@ namespace PontoFidelidade.Persistence
     {
         private readonly Dictionary<int, Loja> Lojas = new Dictionary<int, Loja>();
         private readonly Dictionary<int, Cliente> Clientes = new Dictionary<int, Cliente>();
-        private readonly Dictionary<int, ClienteMovimentacao> ClientesMovimentacoes = new Dictionary<int, ClienteMovimentacao>();
-        private readonly Dictionary<int, ClientePontoFidelidade> ClientesPontoFidelidade = new Dictionary<int, ClientePontoFidelidade>();
+        private readonly Dictionary<int, Usuario> Usuarios = new Dictionary<int, Usuario>();
 
         public static void Initialize(PontoFidelidadeContexto context)
         {
@@ -28,19 +26,21 @@ namespace PontoFidelidade.Persistence
                 return; // Db has been seeded
             }
 
-            var clients = SeedClients(context);
-            var Lojas = SeedLojas(context);
+            var lojas = SeedLojas(context);
+            var clients = SeedClients(context, lojas.First().LojaId);
         }
-        
+
         public Loja[] SeedLojas(PontoFidelidadeContexto context)
         {
             var Lojas = new[]
             {
-                new Loja { LojaId = Guid.NewGuid(), Descricao = "Minhas roupas na moda!", DataCadastro = DateTime.ParseExact("2011-11-01", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture) },
+                new Loja {
+                    LojaId = Guid.NewGuid(), Descricao = "Minhas roupas na moda!", DataCadastro = DateTime.ParseExact("2011-11-01", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture),
+                },
                 new Loja { LojaId = Guid.NewGuid(), Descricao = "Hamburguers top",  DataCadastro = DateTime.ParseExact("2002-02-27", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture)  },
                 new Loja { LojaId = Guid.NewGuid(), Descricao = "Comida Cazeira da Maria",  DataCadastro = DateTime.ParseExact("2019-02-11", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture)  },
                 new Loja { LojaId = Guid.NewGuid(), Descricao = "Loja da Vittoria",  DataCadastro = DateTime.ParseExact("2018-05-08", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture) },
-                new Loja { LojaId = Guid.NewGuid(), Descricao = "Periféricos .Com",  DataCadastro = DateTime.ParseExact("2018-05-30", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture)  },
+                new Loja { LojaId = Guid.NewGuid(), Descricao = "Periféricos PontoCom",  DataCadastro = DateTime.ParseExact("2018-05-30", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture)  },
                 new Loja { LojaId = Guid.NewGuid(), Descricao = "Posto MeuAmigo", DataCadastro = DateTime.ParseExact("2015-10-10", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture)  },
             };
 
@@ -49,18 +49,15 @@ namespace PontoFidelidade.Persistence
 
             return Lojas;
         }
-        public Cliente[] SeedClients(PontoFidelidadeContexto context)
+        public Cliente[] SeedClients(PontoFidelidadeContexto context, Guid idLoja)
         {
             var clients = new[]
             {
-                new Cliente { ClienteId = Guid.NewGuid(), Nome = "Maria Anders", DataCadastro = DateTime.ParseExact("1979-11-01", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture) },
-                new Cliente { ClienteId = Guid.NewGuid(), Nome = "Ana Trujillo",  DataCadastro = DateTime.ParseExact("1993-02-27", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture)  },
-                new Cliente { ClienteId = Guid.NewGuid(), Nome = "Antonio Moreno",  DataCadastro = DateTime.ParseExact("1995-12-11", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture)  },
-                new Cliente { ClienteId = Guid.NewGuid(), Nome = "Thomas Hardy",  DataCadastro = DateTime.ParseExact("1990-05-08", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture) },
-                new Cliente { ClienteId = Guid.NewGuid(), Nome = "Christina Berglund",  DataCadastro = DateTime.ParseExact("1980-05-30", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture)  },
-                new Cliente { ClienteId = Guid.NewGuid(), Nome = "Hanna Moos", DataCadastro = DateTime.ParseExact("1982-10-10", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture),  },
-                new Cliente { ClienteId = Guid.NewGuid(), Nome = "Frédérique Citeaux", DataCadastro = DateTime.ParseExact("1999-06-20", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture)  },
-                new Cliente { ClienteId = Guid.NewGuid(), Nome = "Martín Sommer", DataCadastro = DateTime.ParseExact("1971-01-08", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture)  },
+                ClienteNovo(idLoja, "Maria Anders","1979-11-01", "67845803030"),
+                ClienteNovo(idLoja, "Ana Trujillo", "1993-02-27", "71734065010", 10M, 20),
+                ClienteNovo(idLoja, "Antonio Moreno", "1995-12-11", "81244982024", 2M, 50),
+                ClienteNovo(idLoja, "Thomas Hardy", "1990-05-08", "73793266001", 5M, 200) ,
+                ClienteNovo(idLoja, "Christina Berglund", "1980-05-30", "11972287052", 10M, 100),
             };
 
             context.Cliente.AddRange(clients);
@@ -68,6 +65,52 @@ namespace PontoFidelidade.Persistence
 
             return clients;
         }
+
+        private Cliente ClienteNovo(
+            Guid idLoja,
+            string nome,
+            string dataNascimento = null,
+            string cnpj = null,
+            decimal? valorCredito = 5M,
+            long? pontos = 100
+            )
+        {
+            var id = Guid.NewGuid();
+            return new Cliente
+            {
+                ClienteId = id,
+                Nome = nome,
+                DataCadastro = DateTime.Now,
+                DataNascimento = !string.IsNullOrEmpty(dataNascimento) ? 
+                    DateTime.ParseExact(dataNascimento, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture) : DateTime.MinValue,
+                MovimentacoesDinheiro = new List<ClienteMovimentacao>()
+                {
+                    new ClienteMovimentacao()
+                    {
+                        SaldoAtual = valorCredito ?? 5M,
+                        Valor = valorCredito ?? 5M,
+                        DataOperacao = DateTime.Today,
+                        Operacao = Domain.Models.Enums.Operacao.Credito,
+                        IdCliente = id,
+                        ClienteMovimentacaoId = Guid.NewGuid(),
+                        IdLoja = idLoja
+                    }
+                },
+                MovimentacoesPontoFidelidade= new List<ClientePontoFidelidade>()
+                {
+                    new ClientePontoFidelidade()
+                    {
+                        SaldoAtual = pontos ?? 100,
+                        Pontos = pontos ?? 100,
+                        DataOperacao = DateTime.Today,
+                        Operacao = Domain.Models.Enums.Operacao.Credito,
+                        IdCliente = id,
+                        ClientePontoFidelidadeId = Guid.NewGuid(),
+                        IdLoja = idLoja
+                    }
+                }
+            };          
+		}
 
     }
 }
--- PontoFidelidadeService/PontoFidelidadeService/Program.cs
+++ PontoFidelidadeService/PontoFidelidadeService/Program.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace PontoFidelidadeService
@@ -14,7 +16,25 @@ namespace PontoFidelidadeService
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    var context = scope.ServiceProvider.GetService<PontoFidelidade.Persistence.PontoFidelidadeContexto>();
+                    context.Database.Migrate();
+
+                    PontoFidelidade.Persistence.PontoFidelidadeSeed.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating or initializing the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
