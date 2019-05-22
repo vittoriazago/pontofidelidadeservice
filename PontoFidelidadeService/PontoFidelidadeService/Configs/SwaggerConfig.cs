a94628c71ee3e0f584e0711442f7d285fc4b3279
Vittoria Zago <vittoriazago1@hotmail.com>
2019-05-22 00:01:55 -0300

    Extensão para swagger

---
+++ PontoFidelidadeService/PontoFidelidadeService/Configs/SwaggerConfig.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PontoFidelidade.WebApi
+{
+    public static class SwaggerConfig
+    {
+        public static void ConfigureSwaggerService(this IServiceCollection services)
+        {
+            // Configurando o serviço de documentação do Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "Ponto Fidelidade Service",
+                        Version = "v1",
+                        Description = "Service representing ASPNET CORE",
+                        Contact = new Contact
+                        {
+                            Name = "Vittoria Zago",
+                            Url = "https://github.com/vittoriazago/"
+                        }
+                    });
+
+                string caminhoAplicacao =
+                    PlatformServices.Default.Application.ApplicationBasePath;
+                string nomeAplicacao =
+                    PlatformServices.Default.Application.ApplicationName;
+                string caminhoXmlDoc =
+                    Path.Combine(caminhoAplicacao, $"{nomeAplicacao}.xml");
+
+                c.IncludeXmlComments(caminhoXmlDoc);
+                c.OperationFilter<AuthorizationFilter>();
+            });
+        }
+        public static void ConfigureSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                    "Ponto Fidelidade Service");
+            });
+        }
+
+        class AuthorizationFilter : IOperationFilter
+        {
+            public void Apply(Operation operation, OperationFilterContext context)
+            {
+                if (operation.Parameters == null)
+                    operation.Parameters = new List<IParameter>();
+
+                operation.Parameters.Add(new NonBodyParameter()
+                {
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "string",
+                    Required = false
+                });
+            }
+
+            class HeaderParameter : NonBodyParameter
+            {
+            }
+        }
+    }
+}
